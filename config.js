846a77e31553cee9e5f4367f940e818e1e96aa91
midesofek <midesofek@gmail.com>
2023-08-26 20:34:52 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const API_URL = "https://randomuser.me/api/";
+const NO_OF_USERS = 10;
+const STREAM_ID = "0x1339514086fc15c5e38af4e0407c469ca3911992/test/stream";
+const TIME_INTERVAL = 1000 * 60 * 5; // 5 minutes
+
+module.exports = {
+  API_URL,
+  NO_OF_USERS,
+  STREAM_ID,
+  TIME_INTERVAL,
+};
---
+++ locationStream.js
@@ -0,0 +1,78 @@
+const axios = require("axios");
+const { StreamrClient } = require("streamr-client");
+const { NO_OF_USERS, STREAM_ID, TIME_INTERVAL, API_URL } = require("./config");
+
+require("dotenv").config();
+
+// Initialize the Streamr client
+const streamr = new StreamrClient({
+  auth: {
+    privateKey: process.env.PRIVATE_KEY,
+  },
+});
+
+const streamId = STREAM_ID;
+
+/**
+ * @description fetches data from the random user api
+ * @returns {array} an array of "latitude and longitude"
+ * @example { message: [ '46.5509', '-32.6912'] }
+ */
+const fetchSampleData = async () => {
+  try {
+    // gets the data using axios
+    const res = await axios.get(`${API_URL}?results=${NO_OF_USERS}`);
+    const results = await res.data.results;
+    console.log(results.length);
+
+    // loops over the data gotten from the api and
+    // maps them into a new array
+    const user = results.map((user) => {
+      // const firstname = user.name.first;
+      const userLatitude = user.location.coordinates.latitude;
+      const userLongitude = user.location.coordinates.longitude;
+      return [userLatitude, userLongitude];
+    });
+    return user;
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+////////
+/**
+ * @description - calls the fetchSampleData function then publishes each data
+ * to the Streamr Network. This function is called at a "set" interval.
+ */
+const publish = async () => {
+  try {
+    console.log("Publishing............. ");
+
+    const loadTestData = await fetchSampleData();
+
+    loadTestData.forEach(async (testCoords) => {
+      const data = {
+        message: testCoords,
+      };
+
+      console.log(data);
+
+      await streamr.publish(streamId, data, {
+        timestamp: new Date(),
+      });
+    });
+
+    console.log("Publish successful");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+setInterval(publish, TIME_INTERVAL);
+
+/** @description a sample function to subscribe to data published to this stream */
+// streamr.subscribe(streamId, (data, metadata) => {
+//   const timeReceived = new Date(metadata.timestamp).toISOString();
+
+//   console.log("Time: ", timeReceived);
+//   console.log("Data: ", data.message);
+// });
